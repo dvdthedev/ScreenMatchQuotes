8a621a5c15386abe441f425d1fba1e84f4cc6ff0
Deivid Rocha <deividzor@gmail.com>
2024-06-18 15:11:06 -0300

    first commit

---
+++ src/main/java/alura/com/example/screenmatch_quotes/ScreenmatchQuotesApplication.java
@@ -0,0 +1,13 @@
+package alura.com.example.screenmatch_quotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchQuotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchQuotesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alura/com/example/screenmatch_quotes/ScreenmatchQuotesApplicationTests.java
@@ -0,0 +1,13 @@
+package alura.com.example.screenmatch_quotes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScreenmatchQuotesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
